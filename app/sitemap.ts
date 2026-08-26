b01a37a705a0242385bd690659bfb43811965966
romainaiuser-collab <romain.ai.user@gmail.com>
2026-08-26 16:51:02 +0200

    Add sitemap function for metadata route

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,12 @@
+import type { MetadataRoute } from "next";
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://finds.ponop.io",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ];
+}
