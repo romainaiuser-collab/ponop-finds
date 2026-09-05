0939220882dc0c429c218f9d17fa347e8f7c38e0
romainaiuser-collab <romain.ai.user@gmail.com>
2026-09-05 13:57:03 +0200

    Fetch tools and kits concurrently in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,26 @@
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import ToolFeed from "../components/ToolFeed";
+import { getPublishedKits } from "../lib/kits";
 import { getPublishedTools } from "../lib/tools";
 
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
-  const tools = await getPublishedTools();
+  const [tools, kits] = await Promise.all([
+    getPublishedTools(),
+    getPublishedKits(),
+  ]);
 
   return (
     <div className="min-h-screen bg-[#FAF8F7] text-[#171717]">
       <Hero />
-      <ToolFeed tools={tools} />
+
+      <ToolFeed
+        tools={tools}
+        kits={kits}
+      />
+
       <Footer />
     </div>
   );
